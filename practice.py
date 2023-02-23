5df23ddbe8460ea93a44dc5595e61c46d6c83946
fkfkfj12 <fkfkfj12@naver.com>
2023-02-24 06:14:04 +0900

    연습2

--- practice.py
+++ practice.py
@@ -1,3 +1,4 @@
+# 
 # print(5)
 # print(-10)
 # print(3.14)
@@ -135,7 +136,7 @@
 # print("{0} 의 비밀번호는 {1} 입니다." .format(url, password))
 
 # 리스트 []
-#지하철 칸별로 10명, 20명, 30명
+# 지하철 칸별로 10명, 20명, 30명
 # subway1 = 10
 # subway2 = 20
 # subway3 = 30
@@ -192,4 +193,142 @@
 # num_list.extend(mix_list)
 # print(num_list)
 
-cabinet = {}
+# cabinet = {3:"유재석",100:"김태호"}
+# print(cabinet[3])
+# print(cabinet[100])
+
+# print(cabinet.get(3))   #[] 일때 없는 값이면 프로그램 종료 .get() 일때 없는 값이면 None 출력후 프로그램 계속됨
+# print(cabinet.get(5,"사용 가능"))  # 5가 없는 값일때 사용가능을 출력
+
+
+# print(3 in cabinet)  #True
+# print(5 in cabinet)  #False
+
+# cabinet = {"A-3":"유재석","B-100":"김태호"}   # 키 값은 정수가 아닌 문자도 가능
+# print(cabinet["A-3"])
+# print(cabinet["B-100"])
+# # 사전
+# # 새 손님
+# print(cabinet)
+# cabinet["A-3"] = "김종국"
+# cabinet["C-20"] = "조세호"
+# print(cabinet)
+
+# # 간 손님
+# del cabinet["A-3"]
+# print(cabinet)
+
+# # key 들만 출력
+# print(cabinet.keys())
+
+# # value 들만 출력
+# print(cabinet.values())
+
+# # key, value 쌍으로 출력
+# print(cabinet.items())
+
+# # 모든값 제거
+# cabinet.clear()
+# print(cabinet)
+
+
+# # 튜플
+# menu = ("돈까쓰","치즈까스")
+# print(menu[0])
+# print(menu[1])
+
+# # menu.add("생선까스")  튜플은 .add 불가능
+
+# # name="김종국"
+# # age=20
+# # hobby = "코딩"
+# # pritn(name, age, hobby)
+
+# (name, age, hobby )= ("김종국", 20 , "코딩")
+# print(name,age,hobby)
+
+# # 집합 (set) , 중복 안됨, 순서 없음
+# my_set= {1,2,3,3,3}
+# print(my_set)
+
+# java = {"유재석", "김태호", "양세형"}
+# python = set(["유재석","박명수"])
+# #교집합 (java와 python 을 모두 할 수 있는 개발자)
+# print(java & python)
+# print(java.intersection(python))
+
+# #합집합 (java 할 수 있거나 python 할 수 있는 개발자)
+# print(java | python)
+# print(java.union(python))
+
+# #차집합 (java 할 수 있지만 python 은 할 줄 모르는 개발자)
+# print(java - python)
+# print(java.difference(python))
+
+# # python 할 줄 아는 사람이 늘어남
+# python.add("김태호")
+# print(python)
+
+# # java를 잊었어요
+# java.remove("김태호")
+# print(java)
+
+# # 자료구조의 변경
+# # 커피숍
+# menu= {"커피","쥬스", "우유"}
+# print(menu,type(menu))
+
+# menu = list(menu)
+# print(menu,type(menu))
+
+# menu = tuple(menu)
+# print(menu,type(menu))
+
+# menu = set(menu)
+# print(menu,type(menu))
+
+# from random import *
+# users = range(1, 21) # 1부터 20까지 숫자를 생성
+# users = list(users) #users 의 타입을 list 로 변경
+
+# shuffle(users)
+# winners = sample(users, 4) # 4명 중에서 1명은 치킨, 3명은 커피
+
+# print("-- 당첨자 발표 --\n 치킨 당첨자 : {0} \n 커피 당첨자 : {1}  \n -- 축하합니다 --".format(winners[0],winners[1:]))
+
+# if
+
+# weather = input("오늘 날씨는 어때요? ")
+# if weather == "비" or weather == "눈":
+#     print("우산을 챙기세요")
+# elif weather == "미세먼지":ㄴ
+#     print("마스크를 챙기세요")
+# else:
+#     print("준비물 필요 없어요.")
+
+# temp = int(input("기온은 어때요? "))
+# if 30 <= temp:
+#     print("너무 더워요. 나가지 마세요")
+# elif 10<= temp and temp < 30:
+#     print("괜찮은 날씨에요")
+# elif 0<= temp < 10:
+#     print("외투를 챙기세요")
+# else:
+#     print("너무 추워요. 나가지 마세요")
+
+# for ( 반복문 )
+# for waiting_no in range(1,5):  # 1,2,3,4
+#     print("대기번호 : {0}" .format(waiting_no))
+
+# starbucks = ["아이어맨", "토르" , "아이템 그루트"]
+# for customer in starbucks:
+#     print("{0}, 커피가 준비되었습니다.".format(customer))
+
+# while
+customer = "토르"
+index = 5
+while index >= 1:
+    print("{0}, 커피가 준비 되었습니다. {1} 번 남았어요" .format(customer, index))
+    index -= 1
+    if index ==0:
+        print("커피는 폐기처분되었습니다.")
